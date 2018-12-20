f703fbd4be515ec8a2743b4e1c7a51b062cb5817
Timo <info@ifeelweb.de>
2018-12-20 09:51:29 +0100

    Update put_my_bash_together.sh

--- put_my_bash_together.sh
+++ put_my_bash_together.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 if [ -d "$HOME" ]; then
     if [ -f "$HOME/.bash_profile" ]; then
+        echo "Creating backup of .bash_profile"
         mv "$HOME/.bash_profile" "$HOME/.bash_profile.bak"
     fi
     echo "Fetching .bash_profile"
     wget -q https://gist.githubusercontent.com/timoreith/dbad36a60dc187fdbf1b6c9b2ebc5509/raw/.bash_profile -O $HOME/.bash_profile
+
+    if [ -f "$HOME/.bash_aliases" ]; then
+        echo "Creating backup of .bash_aliases"
+        mv "$HOME/.bash_aliases" "$HOME/.bash_aliases.bak"
+    fi
     echo "Fetching .bash_aliases"
     wget -q https://gist.githubusercontent.com/timoreith/f8f1ee452bb9000c4df0ce3e5b8a9ced/raw/.bash_aliases -O $HOME/.bash_aliases
+    
     echo "Fetching .bash_prompt"
     wget -q https://raw.githubusercontent.com/timoreith/sexy-bash-prompt/master/.bash_prompt -O $HOME/.bash_prompt
     echo "Fetching .vimrc"
